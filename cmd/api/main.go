48f8afbe5b1607ef38d9d91f3d0d1cc3ebdc8391
John Coleman <jcoleman@pagerduty.com>
2018-12-21 23:46:31 -0800

    add default ENVIRONMENT_NAME if unset

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"os"
+	"fmt"
 
 	"github.com/johncoleman83/cerebrum/pkg/api"
 
@@ -9,7 +11,17 @@ import (
 )
 
 func main() {
-
+	envs := map[string]bool{
+    "dev": true,
+    "stg": true,
+		"prod": true,
+	}
+	// set ENVIRONMENT_NAME to dev if unset
+	if _, err := envs["ENVIRONMENT_NAME"]; err {
+    os.Setenv("ENVIRONMENT_NAME", "dev")
+	}
+	fmt.Println(os.Getenv("ENVIRONMENT_NAME"))
+	
 	cfgPath := flag.String("p", "./cmd/api/conf.local.yaml", "Path to config file")
 	flag.Parse()
 

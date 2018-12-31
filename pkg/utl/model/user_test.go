66298b4d8a1f46eda3665254f2718806f66083a2
David John Coleman II <jcoleman@pagerduty.com>
2018-12-30 19:03:39 -0800

    [ci skip] updated readme and added new tests (#8)

--- pkg/utl/model/user_test.go
+++ pkg/utl/model/user_test.go
@@ -3,7 +3,9 @@ package cerebrum_test
 import (
 	"testing"
 
-	"github.com/johncoleman83/cerebrum/pkg/utl/model"
+	"github.com/stretchr/testify/assert"
+
+	cerebrum "github.com/johncoleman83/cerebrum/pkg/utl/model"
 )
 
 func TestChangePassword(t *testing.T) {
@@ -37,7 +39,29 @@ func TestUpdateLastLogin(t *testing.T) {
 	}
 
 	if user.Token != token {
-		t.Errorf("Token was not changed")
+		t.Errorf("Tooken was not changed")
 
 	}
+}
+
+func TestPaginationLimit(t *testing.T) {
+	reqNegativeLimit := cerebrum.PaginationReq{Limit: -5, Page: 2}
+	expected := &cerebrum.Pagination{Limit: 100, Offset: 200}
+	assert.Equal(t, expected, reqNegativeLimit.NewPagination(), "negative limit should get set to default")
+
+	reqMaxLimit := cerebrum.PaginationReq{Limit: 1001, Page: 2}
+	expected.Limit, expected.Offset = 1000, 2000
+	assert.Equal(t, expected, reqMaxLimit.NewPagination(), "beyond max limit should get set to default")
+
+	reqTooBigLimit := cerebrum.PaginationReq{Limit: 9999999, Page: 2}
+	expected.Limit, expected.Offset = 1000, 2000
+	assert.Equal(t, expected, reqTooBigLimit.NewPagination(), "way beyond max limit should get set to default")
+
+	reqNoChangeAllZeros := cerebrum.PaginationReq{Limit: 0, Page: 0}
+	expected.Limit, expected.Offset = 100, 0
+	assert.Equal(t, expected, reqNoChangeAllZeros.NewPagination(), "zeros should get set to default")
+
+	reqNoChange := cerebrum.PaginationReq{Limit: 95, Page: 25}
+	expected.Limit, expected.Offset = 95, 2375
+	assert.Equal(t, expected, reqNoChange.NewPagination(), "some random offset and limit within the bounds should stay the same")
 }
